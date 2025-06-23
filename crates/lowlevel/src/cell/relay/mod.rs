8864252ff5a3eb16e7f71b2a4c7478453562cc30
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2025-06-23 13:33:02 +0700

    Changes to Relay Cell In Prep for Relay Specialization

--- crates/lowlevel/src/cell/relay/mod.rs
+++ crates/lowlevel/src/cell/relay/mod.rs
@@ -9,6 +9,7 @@ use zerocopy::{
 use super::{
     Cell, CellHeader, CellLike, CellRef, FIXED_CELL_SIZE, FixedCell, TryFromCell, to_fixed,
 };
+use crate::cache::Cachable;
 use crate::errors;
 use crate::private::Sealed;
 
@@ -49,22 +50,22 @@ pub(crate) struct RelayCell {
 pub trait RelayLike: Sealed + AsRef<[u8; FIXED_CELL_SIZE]> + AsMut<[u8; FIXED_CELL_SIZE]> {
     /// Gets command.
     fn command(&self) -> u8 {
-        cast(self).header.command
+        cast_cell(self).header.command
     }
 
     /// Sets command.
     fn set_command(&mut self, value: u8) {
-        cast_mut(self).header.command = value;
+        cast_cell_mut(self).header.command = value;
     }
 
     /// Gets recognized.
     fn recognized(&self) -> [u8; 2] {
-        cast(self).header.recognized
+        cast_cell(self).header.recognized
     }
 
     /// Sets recognized.
     fn set_recognized(&mut self, value: [u8; 2]) {
-        cast_mut(self).header.recognized = value;
+        cast_cell_mut(self).header.recognized = value;
     }
 
     /// Check if cell is recognized.
@@ -74,27 +75,27 @@ pub trait RelayLike: Sealed + AsRef<[u8; FIXED_CELL_SIZE]> + AsMut<[u8; FIXED_CE
 
     /// Gets stream ID.
     fn stream(&self) -> u16 {
-        cast(self).header.stream.get()
+        cast_cell(self).header.stream.get()
     }
 
     /// Sets stream ID.
     fn set_stream(&mut self, value: u16) {
-        cast_mut(self).header.stream.set(value);
+        cast_cell_mut(self).header.stream.set(value);
     }
 
     /// Gets digest.
     fn digest(&self) -> [u8; 4] {
-        cast(self).header.digest
+        cast_cell(self).header.digest
     }
 
     /// Sets digest.
     fn set_digest(&mut self, value: [u8; 4]) {
-        cast_mut(self).header.digest = value;
+        cast_cell_mut(self).header.digest = value;
     }
 
     /// Gets length.
     fn len(&self) -> u16 {
-        cast(self).header.len.get()
+        cast_cell(self).header.len.get()
     }
 
     /// Returns [`true`] if length is 0.
@@ -108,7 +109,7 @@ pub trait RelayLike: Sealed + AsRef<[u8; FIXED_CELL_SIZE]> + AsMut<[u8; FIXED_CE
     ///
     /// Panics if length field is invalid. This can happen if the cell content is encrypted.
     fn data(&self) -> &[u8] {
-        let cell = cast(self);
+        let cell = cast_cell(self);
         &cell.data[..usize::from(cell.header.len.get())]
     }
 
@@ -118,7 +119,7 @@ pub trait RelayLike: Sealed + AsRef<[u8; FIXED_CELL_SIZE]> + AsMut<[u8; FIXED_CE
     ///
     /// Panics if length field is invalid. This can happen if the cell content is encrypted.
     fn data_mut(&mut self) -> &mut [u8] {
-        let cell = cast_mut(self);
+        let cell = cast_cell_mut(self);
         &mut cell.data[..usize::from(cell.header.len.get())]
     }
 
@@ -128,7 +129,7 @@ pub trait RelayLike: Sealed + AsRef<[u8; FIXED_CELL_SIZE]> + AsMut<[u8; FIXED_CE
     ///
     /// Panics if `data` is longer than [`RELAY_DATA_LENGTH`].
     fn set_data(&mut self, data: &[u8]) {
-        let cell = cast_mut(self);
+        let cell = cast_cell_mut(self);
         assert!(
             data.len() <= cell.data.len(),
             "data is too long ({} > {})",
@@ -142,11 +143,11 @@ pub trait RelayLike: Sealed + AsRef<[u8; FIXED_CELL_SIZE]> + AsMut<[u8; FIXED_CE
     }
 }
 
-fn cast<T: ?Sized + RelayLike>(this: &T) -> &RelayCell {
+fn cast_cell<T: ?Sized + RelayLike>(this: &T) -> &RelayCell {
     transmute_ref!(this.as_ref())
 }
 
-fn cast_mut<T: ?Sized + RelayLike>(this: &mut T) -> &mut RelayCell {
+fn cast_cell_mut<T: ?Sized + RelayLike>(this: &mut T) -> &mut RelayCell {
     transmute_mut!(this.as_mut())
 }
 
@@ -234,6 +235,12 @@ impl CellLike for Relay {
 
     fn cell(&self) -> CellRef<'_> {
         CellRef::Fixed(&self.cell)
+    }
+}
+
+impl Cachable for Option<Relay> {
+    fn maybe_into_fixed(self) -> Option<FixedCell> {
+        self?.maybe_into_fixed()
     }
 }
 
@@ -342,6 +349,24 @@ impl AsMut<[u8]> for RelayEarly {
     }
 }
 
+impl From<Relay> for RelayEarly {
+    fn from(v: Relay) -> Self {
+        Self {
+            circuit: v.circuit,
+            cell: v.cell,
+        }
+    }
+}
+
+impl From<RelayEarly> for Relay {
+    fn from(v: RelayEarly) -> Self {
+        Self {
+            circuit: v.circuit,
+            cell: v.cell,
+        }
+    }
+}
+
 impl From<RelayEarly> for Cell {
     fn from(v: RelayEarly) -> Cell {
         Cell::from_fixed(
@@ -383,6 +408,12 @@ impl CellLike for RelayEarly {
 
     fn cell(&self) -> CellRef<'_> {
         CellRef::Fixed(&self.cell)
+    }
+}
+
+impl Cachable for Option<RelayEarly> {
+    fn maybe_into_fixed(self) -> Option<FixedCell> {
+        self?.maybe_into_fixed()
     }
 }
 
@@ -442,5 +473,90 @@ impl RelayEarly {
     /// Unwraps into inner [`FixedCell`].
     pub fn into_inner(self) -> FixedCell {
         self.cell
+    }
+}
+
+/// Helper for wrapping RELAY cell payload.
+#[derive(Debug, Clone, PartialEq, Hash)]
+struct RelayWrapper(FixedCell);
+
+impl Sealed for RelayWrapper {}
+impl RelayLike for RelayWrapper {}
+
+impl AsRef<FixedCell> for RelayWrapper {
+    fn as_ref(&self) -> &FixedCell {
+        &self.0
+    }
+}
+
+impl AsMut<FixedCell> for RelayWrapper {
+    fn as_mut(&mut self) -> &mut FixedCell {
+        &mut self.0
+    }
+}
+
+impl AsRef<[u8; FIXED_CELL_SIZE]> for RelayWrapper {
+    fn as_ref(&self) -> &[u8; FIXED_CELL_SIZE] {
+        self.0.data()
+    }
+}
+
+impl AsMut<[u8; FIXED_CELL_SIZE]> for RelayWrapper {
+    fn as_mut(&mut self) -> &mut [u8; FIXED_CELL_SIZE] {
+        self.0.data_mut()
+    }
+}
+
+impl From<FixedCell> for RelayWrapper {
+    fn from(v: FixedCell) -> Self {
+        Self(v)
+    }
+}
+
+impl From<RelayWrapper> for FixedCell {
+    fn from(v: RelayWrapper) -> FixedCell {
+        v.0
+    }
+}
+
+/// Trait to cast from [`Relay`] cell.
+pub trait TryFromRelay: Sized {
+    /// Checks cell content, take it, then cast it into `Self`.
+    ///
+    /// Returns a [`None`] value if the checks are failed, but can be passed onto the next type.
+    /// Error result only happens when the format of the cell itself is invalid.
+    ///
+    /// # Implementers Note
+    ///
+    /// This method **should** not mutate the cell, it's only allowed to inspect it.
+    /// If the checks are good, then use the [`Option::take`] method to pop cell out of it.
+    /// The cell may not be dropped, only moved.
+    fn try_from_relay(relay: &mut Option<Relay>) -> Result<Option<Self>, errors::CellFormatError>;
+}
+
+/// Convenient function wrapping [`TryFromRelay`].
+pub fn cast<T: TryFromRelay>(
+    relay: &mut Option<Relay>,
+) -> Result<Option<T>, errors::CellFormatError> {
+    T::try_from_relay(relay)
+}
+
+/// Convert cell into [`Relay`] cell.
+pub trait IntoRelay: Sized {
+    /// Convert into [`Relay`] with given circuit ID.
+    fn into_relay(self, circuit: NonZeroU32) -> Relay;
+
+    /// Convert into [`RelayEarly`] with given circuit ID.
+    fn into_relay_early(self, circuit: NonZeroU32) -> RelayEarly {
+        self.into_relay(circuit).into()
+    }
+}
+
+impl IntoRelay for RelayWrapper {
+    fn into_relay(self, circuit: NonZeroU32) -> Relay {
+        Relay {
+            circuit,
+            cell: self.0,
+        }
     }
 }
