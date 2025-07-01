a86a3c92c634c61c3ee595d39e823d537a7b02a2
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2025-07-01 19:47:48 +0700

    Adds PADDING_NEGOTIATE Cell

--- crates/lowlevel/src/cell/padding.rs
+++ crates/lowlevel/src/cell/padding.rs
@@ -1,8 +1,12 @@
 use rand::prelude::*;
+use std::mem::{size_of, transmute};
+
+use zerocopy::byteorder::big_endian::U16;
+use zerocopy::{Immutable, IntoBytes, KnownLayout, TryFromBytes, Unaligned, try_transmute_ref};
 
 use super::{
     Cell, CellHeader, CellLike, CellRef, FIXED_CELL_SIZE, FixedCell, TryFromCell, VariableCell,
-    to_fixed, to_variable,
+    to_fixed, to_fixed_with, to_variable,
 };
 use crate::errors;
 
@@ -197,6 +201,205 @@ impl TryFromCell for VPadding {
     }
 }
 
+#[derive(TryFromBytes, IntoBytes, KnownLayout, Immutable, Unaligned)]
+#[repr(u8)]
+enum PaddingNegotiateData {
+    V0(PaddingNegotiateV0) = 0,
+}
+
+#[derive(TryFromBytes, IntoBytes, KnownLayout, Immutable, Unaligned)]
+#[repr(C)]
+struct PaddingNegotiateV0 {
+    cmd: PaddingNegotiateV0Cmd,
+    lo_ms: U16,
+    hi_ms: U16,
+}
+
+#[derive(TryFromBytes, IntoBytes, KnownLayout, Immutable, Unaligned)]
+#[repr(u8)]
+enum PaddingNegotiateV0Cmd {
+    Stop = 1,
+    Start = 2,
+}
+
+#[derive(TryFromBytes, IntoBytes, KnownLayout, Immutable, Unaligned)]
+#[repr(C)]
+struct PaddingNegotiatePadded {
+    data: PaddingNegotiateData,
+    pad: [u8; const { FIXED_CELL_SIZE - size_of::<PaddingNegotiateData>() }],
+}
+
+/// Represents a PADDING_NEGOTIATE cell.
+#[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
+pub struct PaddingNegotiate(FixedCell);
+
+impl AsRef<FixedCell> for PaddingNegotiate {
+    fn as_ref(&self) -> &FixedCell {
+        &self.0
+    }
+}
+
+impl AsMut<FixedCell> for PaddingNegotiate {
+    fn as_mut(&mut self) -> &mut FixedCell {
+        &mut self.0
+    }
+}
+
+impl From<PaddingNegotiate> for Cell {
+    fn from(v: PaddingNegotiate) -> Cell {
+        Cell::from_fixed(CellHeader::new(0, PaddingNegotiate::ID), v.into_inner())
+    }
+}
+
+impl From<PaddingNegotiate> for FixedCell {
+    fn from(v: PaddingNegotiate) -> FixedCell {
+        v.into_inner()
+    }
+}
+
+impl TryFromCell for PaddingNegotiate {
+    fn try_from_cell(cell: &mut Option<Cell>) -> Result<Option<Self>, errors::CellFormatError> {
+        let Some(
+            c @ Cell {
+                command: Self::ID, ..
+            },
+        ) = cell.as_ref()
+        else {
+            return Ok(None);
+        };
+        if c.circuit != 0 {
+            return Err(errors::CellFormatError);
+        }
+        to_fixed_with(cell, Self::check).map(|v| v.map(|v| unsafe { Self::from_cell(v) }))
+    }
+}
+
+impl CellLike for PaddingNegotiate {
+    fn circuit(&self) -> u32 {
+        0
+    }
+
+    fn command(&self) -> u8 {
+        Self::ID
+    }
+
+    fn cell(&self) -> CellRef<'_> {
+        CellRef::Fixed(&self.0)
+    }
+}
+
+impl PaddingNegotiate {
+    /// PADDING_NEGOTIATE command ID.
+    pub const ID: u8 = 12;
+
+    /// Create new PADDING_NEGOTIATE cell from existing [`FixedCell`].
+    ///
+    /// # Safety
+    ///
+    /// Data must be a valid PADDING_NEGOTIATE cell.
+    pub unsafe fn from_cell(data: FixedCell) -> Self {
+        debug_assert!(Self::check(data.data()));
+        Self(data)
+    }
+
+    /// Creates new PADDING_NEGOTIATE cell.
+    ///
+    /// # Parameters
+    ///
+    /// - `cell` : Cached [`FixedCell`].
+    /// - `cmd` : Padding negotiation command.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use onioncloud_lowlevel::cell::padding::{PaddingNegotiate, NegotiateCommand, NegotiateCommandV0};
+    ///
+    /// let cell = PaddingNegotiate::new(Default::default(), NegotiateCommand::V0(NegotiateCommandV0::Stop));
+    /// ```
+    pub fn new(cell: FixedCell, cmd: NegotiateCommand) -> Self {
+        let mut ret = Self(cell);
+        ret.set(cmd);
+        ret
+    }
+
+    /// Get padding negotiation command.
+    pub fn get(&self) -> NegotiateCommand {
+        // SAFETY: Data validity has been checked.
+        let data =
+            unsafe { transmute::<&[u8; FIXED_CELL_SIZE], &PaddingNegotiatePadded>(self.0.data()) };
+        match data.data {
+            PaddingNegotiateData::V0(PaddingNegotiateV0 {
+                cmd: PaddingNegotiateV0Cmd::Stop,
+                ..
+            }) => NegotiateCommand::V0(NegotiateCommandV0::Stop),
+            PaddingNegotiateData::V0(PaddingNegotiateV0 {
+                cmd: PaddingNegotiateV0Cmd::Start,
+                lo_ms,
+                hi_ms,
+            }) => NegotiateCommand::V0(NegotiateCommandV0::Start {
+                low: lo_ms.get(),
+                high: hi_ms.get(),
+            }),
+        }
+    }
+
+    /// Set padding negotiation command.
+    pub fn set(&mut self, cmd: NegotiateCommand) {
+        let data = match cmd {
+            NegotiateCommand::V0(cmd) => PaddingNegotiateData::V0(match cmd {
+                NegotiateCommandV0::Stop => PaddingNegotiateV0 {
+                    cmd: PaddingNegotiateV0Cmd::Stop,
+                    lo_ms: 0.into(),
+                    hi_ms: 0.into(),
+                },
+                NegotiateCommandV0::Start { low, high } => PaddingNegotiateV0 {
+                    cmd: PaddingNegotiateV0Cmd::Start,
+                    lo_ms: low.into(),
+                    hi_ms: high.into(),
+                },
+            }),
+        };
+        let data = data.as_bytes();
+        self.0.data_mut()[..data.len()].copy_from_slice(data);
+        debug_assert!(Self::check(self.0.data()));
+    }
+
+    /// Unwraps into inner [`FixedCell`].
+    pub fn into_inner(self) -> FixedCell {
+        self.0
+    }
+
+    fn check(data: &[u8; FIXED_CELL_SIZE]) -> bool {
+        let Ok(PaddingNegotiatePadded { .. }) = try_transmute_ref!(data) else {
+            return false;
+        };
+        true
+    }
+}
+
+/// Padding negotiate commands.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[non_exhaustive]
+pub enum NegotiateCommand {
+    /// Version 0 commands.
+    V0(NegotiateCommandV0),
+}
+
+/// Padding negotiate commands (version 0).
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum NegotiateCommandV0 {
+    /// Stop padding.
+    Stop,
+
+    /// Start padding.
+    Start {
+        /// Low end of padding interval, in ms.
+        low: u16,
+        /// High end of padding interval, in ms.
+        high: u16,
+    },
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -208,6 +411,17 @@ mod tests {
         fn test_vpadding_size(length in any::<u16>()) {
             let cell = VPadding::with_size(length);
             assert_eq!(cell.data().len(), length as usize);
+        }
+
+        #[test]
+        fn test_padding_negotiate(
+            data in prop_oneof![
+                Just(()).prop_map(|_| NegotiateCommand::V0(NegotiateCommandV0::Stop)),
+                (any::<u16>(), any::<u16>()).prop_map(|(low, high)| NegotiateCommand::V0(NegotiateCommandV0::Start { low, high })),
+            ],
+        ) {
+            let cell = PaddingNegotiate::new(Default::default(), data.clone());
+            assert_eq!(cell.get(), data);
         }
     }
 }
