46f665a7dcd56ff0fa58033eaf44e61ecbc0d648
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2026-01-08 09:52:42 +0700

    Moves NetdocParseError

--- crates/lowlevel/src/errors/complex.rs
+++ crates/lowlevel/src/errors/complex.rs
@@ -3,6 +3,16 @@ use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
 use thiserror::Error;
 
+macro_rules! display2debug {
+    ($i:ident) => {
+        impl Debug for $i {
+            fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+                Display::fmt(self, f)
+            }
+        }
+    };
+}
+
 macro_rules! wraps_enum {
     ($(#[$meta:meta])* $v_outer:vis $outer:ident : $v_inner:vis $inner:ident {$($data:tt)*}) => {
         $(#[$meta])*
@@ -65,3 +75,76 @@ pub CircuitProtocolError: pub(crate) CircuitProtocolInner {
     #[error("SENDME digest mismatch")]
     SendmeDigest,
 }}
+
+#[derive(Error)]
+pub(crate) enum NetdocParseErrorType {
+    #[error("invalid keyword character")]
+    InvalidKeywordChar,
+    #[error("invalid argument character")]
+    InvalidArgumentChar,
+    #[error("invalid object format")]
+    InvalidObjectFormat,
+    #[error("invalid object content")]
+    InvalidObjectContent,
+    #[error("no newline found")]
+    NoNewline,
+    #[error("null character")]
+    Null,
+    #[error("document is empty")]
+    Empty,
+}
+
+display2debug!(NetdocParseErrorType);
+
+enum NetdocPos {
+    Unknown,
+    LinePos { line: isize, pos: usize },
+    ByteOff { off: usize },
+}
+
+impl Display for NetdocPos {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self {
+            Self::LinePos { line, pos } => write!(f, "line {line} byte {pos}"),
+            Self::ByteOff { off } => write!(f, "byte offset {off}"),
+            Self::Unknown => write!(f, "unknown position"),
+        }
+    }
+}
+
+/// Netdoc parsing error.
+#[derive(Error)]
+#[error("error parsing netdoc at {pos}: {reason}")]
+pub struct NetdocParseError {
+    pos: NetdocPos,
+    reason: NetdocParseErrorType,
+}
+
+display2debug! {NetdocParseError}
+
+impl NetdocParseError {
+    pub(crate) const fn with_line_pos(
+        line: isize,
+        pos: usize,
+        reason: NetdocParseErrorType,
+    ) -> Self {
+        Self {
+            reason,
+            pos: NetdocPos::LinePos { line, pos },
+        }
+    }
+
+    pub(crate) const fn with_byte_off(off: usize, reason: NetdocParseErrorType) -> Self {
+        Self {
+            reason,
+            pos: NetdocPos::ByteOff { off },
+        }
+    }
+
+    pub(crate) const fn with_unknown_pos(reason: NetdocParseErrorType) -> Self {
+        Self {
+            reason,
+            pos: NetdocPos::Unknown,
+        }
+    }
+}
