5521fee77d3cf36d099a33cae0ea33b9117df334
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2026-02-04 12:50:24 +0700

    Fix Aach64 Implementation

--- crates/lowlevel/src/parse/netdoc/check/aarch64/mod.rs
+++ crates/lowlevel/src/parse/netdoc/check/aarch64/mod.rs
@@ -6,10 +6,15 @@ mod neon;
 #[cfg(target_arch = "aarch64")]
 use std::arch::aarch64 as arch;
 use std::arch::is_aarch64_feature_detected;
+use std::fmt::{Debug, Formatter, Result as FmtResult};
+use std::marker::PhantomData;
+use std::mem::transmute;
 use std::ptr::{NonNull, null_mut};
 use std::sync::atomic::{AtomicPtr, Ordering::*};
 
-use super::{FPtrs, universal};
+use super::universal;
+
+type FPtrs = super::FPtrs<IterUnion>;
 
 static FP: AtomicPtr<FPtrs> = AtomicPtr::new(null_mut());
 
@@ -23,18 +28,32 @@ fn get_fptr() -> NonNull<FPtrs> {
         static FP: FPtrs = unsafe {
             FPtrs {
                 check_line: |v| neon::check_line(v),
+                proto_keyword: |v| neon::proto_keyword(v),
+                next_non_ws: |v| neon::next_non_ws(v),
                 check_argument: |v| neon::check_argument(v),
                 check_object_keyword: |v| neon::check_object_keyword(v),
                 check_object_content: |v| neon::check_object_content(v),
+                new: |v| neon::new(v),
+                fmt: |v, f| neon::fmt(v, f),
+                next: |v| neon::next(v),
+                size_hint: |v| neon::size_hint(v),
+                next_back: |v| neon::next_back(v),
             }
         };
         &FP
     } else {
         static FP: FPtrs = FPtrs {
             check_line: universal::check_line,
+            proto_keyword: universal::proto_keyword,
+            next_non_ws: universal::next_non_ws,
             check_argument: universal::check_argument,
             check_object_keyword: universal::check_object_keyword,
             check_object_content: universal::check_object_content,
+            new,
+            fmt,
+            next,
+            size_hint,
+            next_back,
         };
         &FP
     });
@@ -45,6 +64,14 @@ fn get_fptr() -> NonNull<FPtrs> {
 
 pub(crate) fn check_line(s: &str) -> Result<usize, usize> {
     unsafe { (get_fptr().as_ref().check_line)(s) }
+}
+
+pub(crate) fn proto_keyword(s: &str) -> Result<usize, usize> {
+    unsafe { (get_fptr().as_ref().proto_keyword)(s) }
+}
+
+pub(crate) fn next_non_ws(s: &str) -> usize {
+    unsafe { (get_fptr().as_ref().next_non_ws)(s) }
 }
 
 pub(crate) fn check_argument(s: &str) -> Option<usize> {
@@ -59,11 +86,85 @@ pub(crate) fn check_object_content(s: &str) -> Option<usize> {
     unsafe { (get_fptr().as_ref().check_object_content)(s) }
 }
 
+fn new(s: &str) -> IterUnion {
+    IterUnion {
+        universal: unsafe { transmute::<&str, &'static str>(s).into() },
+    }
+}
+
+fn fmt(it: NonNull<IterUnion>, f: &mut Formatter<'_>) -> FmtResult {
+    unsafe { Debug::fmt(&(*it.as_ptr()).universal, f) }
+}
+
+fn next(it: NonNull<IterUnion>) -> Option<NonNull<str>> {
+    unsafe { (*it.as_ptr()).universal.next().map(NonNull::from) }
+}
+
+fn size_hint(it: NonNull<IterUnion>) -> (usize, Option<usize>) {
+    unsafe { (*it.as_ptr()).universal.size_hint() }
+}
+
+fn next_back(it: NonNull<IterUnion>) -> Option<NonNull<str>> {
+    unsafe { (*it.as_ptr()).universal.next_back().map(NonNull::from) }
+}
+
+#[derive(Copy)]
+union IterUnion {
+    universal: universal::ArgIterInner<'static>,
+    neon: neon::ArgIterInner,
+}
+
+impl Clone for IterUnion {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+#[derive(Clone, Copy)]
+pub(crate) struct ArgIterInner<'a> {
+    inner: IterUnion,
+    _p: PhantomData<&'a str>,
+}
+
+impl<'a> From<&'a str> for ArgIterInner<'a> {
+    fn from(s: &'a str) -> Self {
+        Self {
+            inner: unsafe { (get_fptr().as_ref().new)(s) },
+            _p: PhantomData,
+        }
+    }
+}
+
+impl Debug for ArgIterInner<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        unsafe { (get_fptr().as_ref().fmt)((&self.inner).into(), f) }
+    }
+}
+
+impl<'a> Iterator for ArgIterInner<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe { (get_fptr().as_ref().next)((&self.inner).into()).map(|v| &*v.as_ptr()) }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        unsafe { (get_fptr().as_ref().size_hint)((&self.inner).into()) }
+    }
+}
+
+impl<'a> DoubleEndedIterator for ArgIterInner<'a> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        unsafe { (get_fptr().as_ref().next_back)((&self.inner).into()).map(|v| &*v.as_ptr()) }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     use super::super::generic as reference;
+    use super::super::tests::{aligned_arg_str, aligned_str};
 
     use proptest::prelude::*;
 
@@ -72,10 +173,38 @@ mod tests {
         let is_neon = is_aarch64_feature_detected!("neon");
 
         if is_neon {
-            proptest!(|(s: String)| {
+            proptest!(|((s, _o) in aligned_str())| {
                 let r = reference::check_line(&s);
                 if is_neon {
                     assert_eq!(r, unsafe { neon::check_line(&s) });
+                }
+            });
+        }
+    }
+
+    #[test]
+    fn test_proto_keyword() {
+        let is_neon = is_aarch64_feature_detected!("neon");
+
+        if is_neon {
+            proptest!(|((s, _o) in aligned_str())| {
+                let r = reference::proto_keyword(&s);
+                if is_neon {
+                    assert_eq!(r, unsafe { neon::proto_keyword(&s) });
+                }
+            });
+        }
+    }
+
+    #[test]
+    fn test_next_non_ws() {
+        let is_neon = is_aarch64_feature_detected!("neon");
+
+        if is_neon {
+            proptest!(|((s, _o) in aligned_str())| {
+                let r = reference::next_non_ws(&s);
+                if is_neon {
+                    assert_eq!(r, unsafe { neon::next_non_ws(&s) });
                 }
             });
         }
@@ -86,7 +215,7 @@ mod tests {
         let is_neon = is_aarch64_feature_detected!("neon");
 
         if is_neon {
-            proptest!(|(s: String)| {
+            proptest!(|((s, _o) in aligned_str())| {
                 let r = reference::check_argument(&s);
                 if is_neon {
                     assert_eq!(r, unsafe { neon::check_argument(&s) });
@@ -100,7 +229,7 @@ mod tests {
         let is_neon = is_aarch64_feature_detected!("neon");
 
         if is_neon {
-            proptest!(|(s: String)| {
+            proptest!(|((s, _o) in aligned_str())| {
                 let r = reference::check_object_keyword(&s);
                 if is_neon {
                     assert_eq!(r, unsafe { neon::check_object_keyword(&s) });
@@ -114,10 +243,46 @@ mod tests {
         let is_neon = is_aarch64_feature_detected!("neon");
 
         if is_neon {
-            proptest!(|(s: String)| {
+            proptest!(|((s, _o) in aligned_str())| {
                 let r = reference::check_object_content(&s);
                 if is_neon {
                     assert_eq!(r, unsafe { neon::check_object_content(&s) });
+                }
+            });
+        }
+    }
+
+    #[test]
+    fn test_arg_iter() {
+        let is_neon = is_aarch64_feature_detected!("neon");
+
+        if is_neon {
+            proptest!(|((s, _o) in aligned_arg_str())| unsafe {
+                //dbg!(_o, s.as_bytes());
+                if is_neon {
+                    let mut it = neon::new(&s);
+                    for (i, r) in reference::ArgIterInner::from(&*s).enumerate() {
+                        let Some(t) = neon::next((&mut it).into()).map(|s| &*s.as_ptr()) else {
+                            panic!("iterator should produce at least {i} items")
+                        };
+                        //dbg!(t);
+                        assert_eq!(r, t, "mismatch at index {i}");
+                    }
+                    if let Some(t) = neon::next((&mut it).into()).map(|s| &*s.as_ptr()) {
+                        panic!("iterator should stop, got {t:?}")
+                    }
+
+                    let mut it = neon::new(&s);
+                    for (i, r) in reference::ArgIterInner::from(&*s).rev().enumerate() {
+                        let Some(t) = neon::next_back((&mut it).into()).map(|s| &*s.as_ptr()) else {
+                            panic!("iterator should produce at least {i} items")
+                        };
+                        //dbg!(t);
+                        assert_eq!(r, t, "mismatch at index {i}");
+                    }
+                    if let Some(t) = neon::next_back((&mut it).into()).map(|s| &*s.as_ptr()) {
+                        panic!("iterator should stop, got {t:?}")
+                    }
                 }
             });
         }
--- crates/lowlevel/src/parse/netdoc/check/aarch64/neon.rs
+++ crates/lowlevel/src/parse/netdoc/check/aarch64/neon.rs
@@ -51,7 +51,7 @@ pub(crate) fn check_line(s: &str) -> Result<usize, usize> {
         debug_assert_ne!(c.len(), usize::MAX);
 
         // Main
-        while i + 15 < c.len() {
+        while i + 16 <= c.len() {
             let v = arch::vld1q_u8(get_unchecked(c, i));
 
             if i == 0 && arch::vgetq_lane_u8(v, 0) == 0x2d {
@@ -114,7 +114,79 @@ pub(crate) fn check_line(s: &str) -> Result<usize, usize> {
 }
 
 #[target_feature(enable = "neon")]
-pub(crate) fn check_argument(s: &str) -> Option<usize> {
+pub(crate) fn proto_keyword(s: &str) -> Result<usize, usize> {
+    // SAFETY: It's more ergonomic to wrap the entire function in unsafe :D
+    unsafe {
+        let mut i = 0usize;
+        let c = from_ref(s.as_bytes());
+
+        // Should be impossible even with maximal allocation.
+        // Rust don't allow pointer offset > isize::MAX.
+        // But we do sanity check anyway.
+        debug_assert_ne!(c.len(), usize::MAX);
+
+        // Main
+        while i + 16 <= c.len() {
+            let v = arch::vld1q_u8(get_unchecked(c, i));
+
+            if i == 0 && matches!(arch::vgetq_lane_u8(v, 0), b'-' | b'=') {
+                // First character is - or =
+                return Err(0);
+            }
+
+            // =
+            let eq = arch::vceqq_u8(v, arch::vdupq_n_u8(b'='));
+
+            // A-Z and a-z
+            let v_ = arch::vandq_u8(v, arch::vdupq_n_u8(0xdfu8));
+            let mut t = arch::vorrq_u8(
+                arch::vcltq_u8(v_, arch::vdupq_n_u8(0x41)),
+                arch::vcgtq_u8(v_, arch::vdupq_n_u8(0x5a)),
+            );
+
+            // Numbers
+            let n = arch::vorrq_u8(
+                arch::vcltq_u8(v, arch::vdupq_n_u8(0x30)),
+                arch::vcgtq_u8(v, arch::vdupq_n_u8(0x39)),
+            );
+            t = arch::vandq_u8(t, n);
+
+            // -
+            let n = arch::vceqq_u8(v, arch::vdupq_n_u8(b'-'));
+            t = arch::vbicq_u8(t, n);
+
+            let v1 = movemask(t).trailing_zeros();
+            let v2 = movemask(eq).trailing_zeros();
+
+            if v1 < v2 {
+                // Non-matching character before =
+                return Err(i + v1 as usize);
+            } else if v2 < 16 {
+                // =
+                return Ok(i + v2 as usize);
+            }
+            debug_assert_eq!(v1, 16);
+            debug_assert_eq!(v2, 16);
+
+            i += 16;
+        }
+
+        // Tail
+        for i in i..c.len() {
+            match *get_unchecked(c, i) {
+                b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' => (),
+                b'-' if i != 0 => (),
+                b' ' | b'\t' => return Ok(i),
+                _ => return Err(i),
+            }
+        }
+    }
+
+    Ok(s.len())
+}
+
+#[target_feature(enable = "neon")]
+pub(crate) fn next_non_ws(s: &str) -> Option<usize> {
     // SAFETY: It's more ergonomic to wrap the entire function in unsafe :D
     unsafe {
         let mut i = 0usize;
@@ -127,28 +199,14 @@ pub(crate) fn check_argument(s: &str) -> Option<usize> {
 
         // Main
         let mut sp = true;
-        while i + 15 < c.len() {
+        while i + 16 <= c.len() {
             let v = arch::vld1q_u8(get_unchecked(c, i));
 
-            // Null and newline test
-            let z = movemask(arch::vorrq_u8(
-                arch::vceqq_u8(v, arch::vdupq_n_u8(0)),
-                arch::vceqq_u8(v, arch::vdupq_n_u8(b'\n')),
-            ));
-
             // Space and tab
-            let t = arch::vorrq_u8(
-                arch::vceqq_u8(v, arch::vdupq_n_u8(b' ')),
-                arch::vceqq_u8(v, arch::vdupq_n_u8(b'\t')),
-            );
-            let mut t = movemask(t);
-
-            // Two space or tab
-            let t_ = t << 1 | sp as u16;
-            sp = (t >> 15) != 0;
-            t &= t_;
-            t |= z;
-
+            let t = movemask(arch::vandq_u8(
+                arch::vcntq_u8(v, arch::vdupq_n_u8(b' ')),
+                arch::vcntq_u8(v, arch::vdupq_n_u8(b'\t')),
+            ));
             if let v @ 0..=15 = t.trailing_zeros() {
                 return Some(i + v as usize);
             }
@@ -158,16 +216,61 @@ pub(crate) fn check_argument(s: &str) -> Option<usize> {
 
         // Tail
         for i in i..c.len() {
-            sp = match (*get_unchecked(c, i), sp) {
-                (b'\0' | b'\n', _) => return Some(i),
-                (b' ' | b'\t', false) => true,
-                (b' ' | b'\t', true) => return Some(i),
-                _ => false,
-            };
+            if !matches!(*get_unchecked(c, i), b' ' | b'\t') {
+                return Some(i);
+            }
+        }
+    }
+
+    s.len()
+}
+
+#[target_feature(enable = "neon")]
+pub(crate) fn check_argument(s: &str) -> Option<usize> {
+    if matches!(s.as_bytes().first(), Some(b' ' | b'\t')) {
+        return Some(0);
+    }
+
+    // SAFETY: It's more ergonomic to wrap the entire function in unsafe :D
+    unsafe {
+        let mut i = 0usize;
+        let c = from_ref(s.as_bytes());
+
+        // Should be impossible even with maximal allocation.
+        // Rust don't allow pointer offset > isize::MAX.
+        // But we do sanity check anyway.
+        debug_assert_ne!(c.len(), usize::MAX);
+
+        // Main
+        let mut sp = true;
+        while i + 16 <= c.len() {
+            let v = arch::vld1q_u8(get_unchecked(c, i));
+
+            // Null and newline test
+            let t = movemask(arch::vorrq_u8(
+                arch::vceqq_u8(v, arch::vdupq_n_u8(0)),
+                arch::vceqq_u8(v, arch::vdupq_n_u8(b'\n')),
+            ));
+            if let v @ 0..=15 = t.trailing_zeros() {
+                return Some(i + v as usize);
+            }
+
+            i += 16;
         }
 
-        if sp { Some(s.len()) } else { None }
+        // Tail
+        for i in i..c.len() {
+            if matches!(*get_unchecked(c, i), b'\0' | b'\n') {
+                return Some(i);
+            }
+        }
     }
+
+    if matches!(s.as_bytes().last(), Some(b' ' | b'\t')) {
+        return Some(s.len() - 1);
+    }
+
+    None
 }
 
 #[target_feature(enable = "neon")]
@@ -184,7 +287,7 @@ pub(crate) fn check_object_keyword(s: &str) -> Option<usize> {
 
         // Main
         let mut sp = true;
-        while i + 15 < c.len() {
+        while i + 16 <= c.len() {
             let v = arch::vld1q_u8(get_unchecked(c, i));
 
             // Space and tab
@@ -248,7 +351,7 @@ pub(crate) fn check_object_content(s: &str) -> Option<usize> {
         debug_assert_ne!(c.len(), usize::MAX);
 
         // Main
-        while i + 15 < c.len() {
+        while i + 16 <= c.len() {
             let v = arch::vld1q_u8(get_unchecked(c, i));
 
             // +, /, =
@@ -284,5 +387,180 @@ pub(crate) fn check_object_content(s: &str) -> Option<usize> {
 
         // Tail
         (i..c.len()).find(|&i| !matches!(*get_unchecked(c, i), b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' | b'+' | b'/' | b'='))
+    }
+}
+
+#[derive(Clone, Copy)]
+pub(crate) struct ArgIterInner {
+    s: &'static str,
+    i: usize,
+    e: usize,
+    bs: u16,
+    be: u16,
+}
+
+impl ArgIterInner {
+    #[target_feature(enable = "neon")]
+    #[inline]
+    fn process_batch(v: *const u8) -> u16 {
+        let v = unsafe { arch::vld1q_u8(v) };
+        // Space and tab
+        movemask(arch::vorrq_u8(
+            arch::vceqq_u8(v, arch::vdup1_n_u8(b' ')),
+            arch::vceqq_u8(v, arch::vdup1_n_u8(b'\t')),
+        ))
+    }
+
+    #[target_feature(enable = "neon")]
+    fn forward(&mut self, ws: bool) {
+        let c = from_ref(self.s.as_bytes());
+        // SAFETY: It's more ergonomic to wrap the entire function in unsafe :D
+        unsafe {
+            // Main
+            while self.i < self.e && (self.i & !15) + 16 <= c.len() {
+                let o = self.i & 15;
+                if o == 0 {
+                    // Load batch
+                    self.bs = Self::process_batch(get_unchecked(c, self.i));
+                }
+
+                let o =
+                    (if ws { self.bs } else { !self.bs } & u16::MAX << o).trailing_zeros() as usize;
+                // If o is 16 it will advance i
+                self.i = (self.i & !15) + o;
+                if o < 16 {
+                    return;
+                }
+            }
+
+            // Tail
+            while self.i < self.e {
+                if matches!(*get_unchecked(c, self.i), b' ' | b'\t') ^ !ws {
+                    return;
+                }
+                self.i += 1;
+            }
+        }
+    }
+
+    #[target_feature(enable = "neon")]
+    fn backward(&mut self, ws: bool) {
+        let c = from_ref(self.s.as_bytes());
+        // SAFETY: It's more ergonomic to wrap the entire function in unsafe :D
+        unsafe {
+            // Head
+            while self.i < self.e && self.e > c.len() & !15 {
+                if matches!(*get_unchecked(c, self.e - 1), b' ' | b'\t') ^ !ws {
+                    return;
+                }
+                self.e -= 1;
+            }
+
+            // Main
+            while self.i < self.e {
+                let o = self.e.wrapping_neg() & 15;
+                if o == 0 {
+                    // Load batch
+                    self.e -= 16;
+                    self.be = Self::process_batch(get_unchecked(c, self.e));
+                }
+
+                let o = (if ws { self.be } else { !self.be } & (u16::MAX >> o)).leading_zeros()
+                    as usize;
+                // If o is 16 it will advance e
+                self.e = (self.e & !15) + (16 - o);
+                if o < 16 {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+#[target_feature(enable = "neon")]
+pub(crate) fn new(s: &str) -> IterUnion {
+    IterUnion {
+        neon: ArgIterInner {
+            s: unsafe { transmute::<&str, &'static str>(s) },
+            i: 0,
+            e: s.len(),
+            bs: 0,
+            be: 0,
+        },
+    }
+}
+
+#[target_feature(enable = "neon")]
+pub(crate) fn fmt(it: NonNull<IterUnion>, f: &mut Formatter<'_>) -> FmtResult {
+    let p = unsafe { &(*it.as_ptr()).neon };
+    f.debug_struct("ArgIterInner")
+        .field("i", &p.i)
+        .field("e", &p.e)
+        .field("bs", &p.bs)
+        .field("be", &p.be)
+        .field("sp", &p.s.get(p.i..p.e))
+        .finish_non_exhaustive()
+}
+
+#[target_feature(enable = "neon")]
+pub(crate) fn next(it: NonNull<IterUnion>) -> Option<NonNull<str>> {
+    // SAFETY: It's more ergonomic to wrap the entire function in unsafe :D
+    unsafe {
+        let p = &mut (*it.as_ptr()).neon;
+
+        let s = p.i;
+        let e = p.e;
+        debug_assert!(s <= e, "{s} > {e}");
+
+        if s >= e {
+            return None;
+        }
+
+        p.forward(true);
+        let m = p.i;
+        debug_assert!(m <= e, "{m} > {e}");
+        debug_assert!(m >= s, "{m} < {s}");
+        if m < e {
+            // Skip whitespace
+            p.forward(false);
+        }
+
+        Some(p.s.get_unchecked(s..m).into())
+    }
+}
+
+#[target_feature(enable = "neon")]
+pub(crate) fn size_hint(it: NonNull<IterUnion>) -> (usize, Option<usize>) {
+    // SAFETY: It's more ergonomic to wrap the entire function in unsafe :D
+    unsafe {
+        let p = &mut (*it.as_ptr()).neon;
+        if p.e <= p.i { (0, Some(0)) } else { (1, None) }
+    }
+}
+
+#[target_feature(enable = "neon")]
+pub(crate) fn next_back(it: NonNull<IterUnion>) -> Option<NonNull<str>> {
+    // SAFETY: It's more ergonomic to wrap the entire function in unsafe :D
+    unsafe {
+        let p = &mut (*it.as_ptr()).neon;
+
+        let s = p.i;
+        let e = p.e;
+        debug_assert!(s <= e, "{s} > {e}");
+
+        if s >= e {
+            return None;
+        }
+
+        p.backward(true);
+        let m = p.e;
+        debug_assert!(m >= s, "{m} < {s}");
+        debug_assert!(m <= e, "{m} > {e}");
+        if m > s {
+            // Skip whitespace
+            p.backward(false);
+        }
+
+        Some(p.s.get_unchecked(m..e).into())
     }
 }
