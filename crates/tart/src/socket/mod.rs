79407a316115e5609900e3e56079adb8f5ba015c
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2026-08-11 21:10:25 +0700

    Adds Tests for Network Socket

--- crates/tart/src/socket/mod.rs
+++ crates/tart/src/socket/mod.rs
@@ -58,6 +58,14 @@ impl Future for Offline {
 mod tests {
     use super::*;
 
+    use std::collections::VecDeque;
+    use std::io::{Error as IoError, IoSlice, IoSliceMut, Read, Write};
+    use std::pin::pin;
+    use std::task::ready;
+    use std::time::Duration;
+
+    use futures_util::{AsyncReadExt, AsyncWriteExt, select_biased};
+    use pin_project::pin_project;
     use test_log::test;
     use tracing::{info, instrument};
 
@@ -84,6 +92,184 @@ mod tests {
                     ErrorKind::NotConnected,
                     "error kind mismatch: {e}"
                 );
+            });
+
+            executor.run();
+        }
+
+        run_test(test);
+    }
+
+    struct NetworkEchoEmulator;
+
+    impl SocketHandler for NetworkEchoEmulator {
+        fn open(&mut self, args: OpenOpt<'_>) -> SocketOpener {
+            let rt = args.rt.clone();
+            let addr = args.addrs.last().copied();
+            Box::pin(async move {
+                Timer::with_duration(rt.clone(), Duration::from_secs(285031)).await;
+                let addr = addr.ok_or(ErrorKind::InvalidInput)?;
+                Ok((addr, Box::pin(Echo::new(rt)) as Socket))
+            })
+        }
+    }
+
+    #[pin_project]
+    struct Echo {
+        buf: VecDeque<u8>,
+        #[pin]
+        timer: Timer,
+    }
+
+    fn timed_out(mut timer: Pin<&mut Timer>, cx: &mut Context<'_>) -> Poll<()> {
+        let ret = timer.as_mut().poll(cx);
+        if ret.is_ready() {
+            timer.set_duration(Duration::from_secs(6 * 60 + 7));
+        }
+        ret
+    }
+
+    impl Echo {
+        fn new(rt: Runtime) -> Self {
+            Self {
+                buf: VecDeque::new(),
+                timer: Timer::with_duration(rt, Duration::from_secs(6 * 60 + 7)),
+            }
+        }
+    }
+
+    impl AsyncRead for Echo {
+        fn poll_read(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buf: &mut [u8],
+        ) -> Poll<IoResult<usize>> {
+            let this = self.project();
+            ready!(timed_out(this.timer, cx));
+
+            Poll::Ready(match this.buf.read(buf) {
+                Ok(0) if !buf.is_empty() => Err(IoError::new(
+                    ErrorKind::UnexpectedEof,
+                    "server does not have more data",
+                )),
+                v => v,
+            })
+        }
+
+        fn poll_read_vectored(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            bufs: &mut [IoSliceMut<'_>],
+        ) -> Poll<IoResult<usize>> {
+            let this = self.project();
+            ready!(timed_out(this.timer, cx));
+
+            let is_empty = bufs.iter().all(|v| v.is_empty());
+            Poll::Ready(match this.buf.read_vectored(bufs) {
+                Ok(0) if !is_empty => Err(IoError::new(
+                    ErrorKind::UnexpectedEof,
+                    "server does not have more data",
+                )),
+                v => v,
+            })
+        }
+    }
+
+    impl AsyncWrite for Echo {
+        fn poll_write(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            buf: &[u8],
+        ) -> Poll<IoResult<usize>> {
+            let this = self.project();
+            ready!(timed_out(this.timer, cx));
+
+            Poll::Ready(this.buf.write(buf))
+        }
+
+        fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<IoResult<()>> {
+            let this = self.project();
+            ready!(timed_out(this.timer, cx));
+
+            Poll::Ready(Ok(()))
+        }
+
+        fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<IoResult<()>> {
+            let this = self.project();
+            ready!(timed_out(this.timer, cx));
+
+            Poll::Ready(Ok(()))
+        }
+
+        fn poll_write_vectored(
+            self: Pin<&mut Self>,
+            cx: &mut Context<'_>,
+            bufs: &[IoSlice<'_>],
+        ) -> Poll<IoResult<usize>> {
+            let this = self.project();
+            ready!(timed_out(this.timer, cx));
+
+            Poll::Ready(this.buf.write_vectored(bufs))
+        }
+    }
+
+    #[test]
+    fn test_socket_echo() {
+        #[instrument]
+        fn test() {
+            let mut executor = Executor::builder()
+                .with_network(NetworkEchoEmulator)
+                .build();
+            let rt = executor.runtime();
+
+            let rt_ = rt.clone();
+            rt_.spawn(async move {
+                let mut socket = {
+                    select_biased! {
+                        v = rt.connect(&[([127, 0, 0, 1u8], 8000).into()]) => pin!(v.unwrap()),
+                        _ = Timer::with_duration(rt.clone(), Duration::from_secs(384146)) => panic!("socket opening timed out"),
+                    }
+                };
+
+                let msg = b"Streng dich an, Nana!";
+
+                for _ in 0..2 {
+                    info!("writing into socket");
+                    socket.as_mut().write_all(msg).await.unwrap();
+
+                    info!("reading from socket");
+                    let mut buf = vec![0; msg.len()];
+                    socket.as_mut().read_exact(&mut buf).await.unwrap();
+                    assert_eq!(buf, msg);
+                }
+
+                info!("done");
+            });
+
+            executor.run();
+        }
+
+        run_test(test);
+    }
+
+    #[test]
+    fn test_socket_open_timeout() {
+        #[instrument]
+        fn test() {
+            let mut executor = Executor::builder()
+                .with_network(NetworkEchoEmulator)
+                .build();
+            let rt = executor.runtime();
+
+            let rt_ = rt.clone();
+            rt_.spawn(async move {
+                let mut fut = pin!(rt.connect(&[([127, 0, 0, 1u8], 8000).into()]));
+                select_biased! {
+                    r = fut => panic!("opening socket should timeout, got {r:?} instead"),
+                    _ = Timer::with_duration(rt.clone(), Duration::from_secs(269835)) => panic!("later timer fires before earlier timer"),
+                    _ = Timer::with_duration(rt.clone(), Duration::from_secs(256895)) => panic!("later timer fires before earlier timer"),
+                    _ = Timer::with_duration(rt.clone(), Duration::from_secs(225475)) => {}
+                }
             });
 
             executor.run();
